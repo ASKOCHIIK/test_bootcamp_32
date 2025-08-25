29ee31738f8e508da22cbdf7b7aabf4baf7a1103
Kubanych <kubanych@Macbookairs-MacBook-Air.local>
2025-08-25 19:18:31 +0600

    new kod

--- lesson.py
+++ lesson.py
@@ -2,3 +2,26 @@ def main():
     print("Hello World")
 if __name__ == "__main__":
     main()
+
+
+
+
+class Student:
+    def __init__(self, age):
+        self.__age = age
+    @property
+    def age(self):
+        return self.__age
+
+    @age.setter
+    def age(self, value):
+        if value < 0:
+            raise ValueError("Жаш терс болбойт")
+        self.__age = value
+s = Student(18)
+print(s.age)
+
+s.age = 20
+print(s.age)
+
+s.age = 5
