c942e23578baaf786777d0117c862af4848cb853
DONIKO312 <a2562656656@gmail.com>
2025-08-25 19:08:27 +0600

    DON1KO

--- lesson.py
+++ lesson.py
@@ -2,3 +2,12 @@ def main():
     print("Hello World")
 if __name__ == "__main__":
     main()
+
+class Doniko:
+    def __init__(self, name):
+        self.name = name
+
+    def Get_info(self):
+        return self.name
+D = Doniko('Daniel')
+print(D.Get_info())
